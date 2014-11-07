a8af724a33daeb1149357e27efe7519b410e0e98
flip <ansuz@transitiontech.ca>
2014-11-07 01:24:57 -0500

    update to reflect changes

--- blag.app.js
+++ blag.app.js
@@ -1,4 +1,6 @@
 var unmon=require("./lib/unmon.js");
 var mds=require("./lib/plug/md.js");
 
-unmon.launch({main:mds.spawnHandler(process.env.PWD+"/md/","unmon")});
+unmon.launch({
+  main:mds(process.env.PWD+"/md/","unmon")
+});
