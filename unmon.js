bc7375f034cd4347d62548a57f725927ac50f1b1
ansuz <ansuz@transitiontech.ca>
2015-02-02 08:48:50 -0500

    route bundles a null-ary "next" function

--- unmon.js
+++ unmon.js
@@ -1,32 +1,27 @@
 var http=require("http");
 
-var caching=require("./lib/cache.js");
-
-var logger=require("./lib/logger.js")();
-
-var cors=require("./lib/cors.js");
-
-var fixed=require("./lib/fixed.js")(process.env.PWD+"/static","index.html");
-
-var blag=require("./lib/blag.js")({
-  path:process.env.PWD+"/md/"
-  ,title:" "
-  ,home:"index"
-  ,debug:false
-  ,pattern:'{\\w+}'
-  ,timeformat:"toISOString" // "getTime", "toJSON", any Date() method
+process.on('uncaughtException',function(err){
+  // this is a really dirty way to keep your server alive
+  // but it's preferable to letting it die
+  // in the future there will be a domain based solution
+  console.log('Caught exception: ' + err);
 });
 
-var nope=require("./lib/nope.js");
-
-var f404=require("./lib/parcel.js")(process.env.PWD+"/static/html/404.html");
-
-/* Support Functions */
+/* Your default stack */
 var routes=[];
 
-var route=function(patt,f){
-  routes.push(function(req,res,next){
-    if(req.url.match(patt)){
+/* route produces new routes, and pushes them to a stack */
+var route=function(patt,f,opts){
+  opts=opts||{}; // you don't have to provide options
+  // make sure the discriminator is defined
+  opts.disc=opts.disc||'url'; // your router parses by url by default
+  opts.stack=opts.stack||routes; // you can provide an alternate stack
+
+  // push your new route to the stack in question
+  opts.stack.push(function(req,res,Next){
+    // encapsulate Next so that we don't always have to pass req and res
+    var next=function(){Next(req,res);};
+    if(patt.test(req[opts.disc])){
       f(req,res,next);
     }else{
       next(req,res);
@@ -34,51 +29,55 @@ var route=function(patt,f){
   });
 };
 
-/* Check filetype and cache */
-route(/.*/,caching);
+// now push some routes!
+
+/* Cache static files */
+route(/.*/,require("./lib/cache"));
 
 /* Enable CORS */
-route(/.*/,cors);
+route(/.*/,require("./lib/cors.js"));
 
 /* Logger */
-route(/.*/,logger);
+route(/.*/,require("./lib/logger.js")({path:process.env.PWD+'/log/'}));
 
-/* root */
-route(/^\//,blag);
+/* Accept post requests */
+route(/.*/,require("./lib/posted.js")());
+
+/* koan */
+route(/.*/,require("./lib/koan.js")());
 
 /* Route static files */
-route(/.*/,fixed);
+route(/.*/,require("./lib/fixed")(process.env.PWD+"/static","index.html"));
 
-/* teh blag */
-route(/.*/,blag);
+/* blag */
+route(/.*/,require("./lib/blag.js")({path:process.env.PWD+"/md/",title:" "}));
 
+/* last resort */
+route(/.*/,require("./lib/parcel.js")(process.env.PWD+"/static/html/404.html"));
 
-
-route(/.*/,f404);
-
-/* 404 Route */
-route(/.*/,nope);
-
-var router=routes
-  .reduceRight(function(b,a,i,z){
+var makeRouter=function(stack){
+  return stack.reduceRight(function(b,a,i,z){
     return function(req,res){
       a(req,res,b);
     };
   });
+};
 
-var port=8083;
-var addresses=[""]; // the array of addresses on which to listen
+var router=makeRouter(routes)
+
+global.thisServersPort=8083;
+var addresses=[''];
 
 (function(){
-  var fc=require("./lib/fc.js")();
-  if(fc)
-    addresses.push(fc[0]);
+  global.thisServersIPV6=require("./lib/fc.js")();
+  if(thisServersIPV6)
+    addresses.push(thisServersIPV6[0]);
 })();
 
 var servers=addresses.map(function(addy){
   var server=http.createServer(router);
-  server.listen(port,addy,function(){
-    console.log("unmon is running on %s:%s",addy,port);
+  server.listen(thisServersPort,addy,function(){
+    console.log("unmon is running on %s:%s",addy,thisServersPort);
   });
   return server;
 });
